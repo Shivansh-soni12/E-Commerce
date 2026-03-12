e33927a5de5b95504444624a4c12083da6996cba
Vedansh2111 <Vedansh.Bhatnagar@cognizant.com>
2026-03-12 13:44:24 +0530

    empty-cart logic

---
+++ Frontend/src/app/components/Add-Cart/empty-cart/empty-cart.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-empty-cart',
+  imports: [RouterLink],
+  templateUrl: './empty-cart.html',
+  styleUrl: './empty-cart.css',
+})
+export class EmptyCart {
+
+}
