d2f223ed05cb2bbb45bc5b82a82719910abc5789
Shivansh-soni12 <shivansh.soni@cognizant.com>
2026-03-16 18:17:06 +0530

    Add files via upload

--- Frontend/src/app/guard/auth.guard.ts
+++ Frontend/src/app/guard/auth.guard.ts
@@ -4,9 +4,18 @@ import { UserService } from '../services/user-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
-  if (userService.getLoggedUserId()) {
-  }
+  const router = inject(Router);
+
+  // Check if the RAM variable exists
+  const token = userService.getToken();
+
+  if (token) {
     return true; 
-  inject(Router).navigate(['/login']);
+  }
+  
+  // LOGGING FOR DEBUGGING
+  console.error('AuthGuard: Access denied for this tab. Redirecting to login.');
+  
+  router.navigate(['/login']);
   return false;
 };
